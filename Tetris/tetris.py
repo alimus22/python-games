361be8cf37944ea5f5ccb2e4e456c22f6a9627f6
Ali Hanni <55854824+alimus22@users.noreply.github.com>
2021-05-11 19:13:49 -0400

    Started tetraminos implementation

--- Tetris/tetris.py
+++ Tetris/tetris.py
@@ -1,6 +1,7 @@
 import pygame
 import math
 import sys
+import random
 from random import *
 
 
@@ -12,7 +13,7 @@ BLUE = (0, 0, 255)
 YELLOW = (255, 230, 0)
 PURPLE = (205, 0, 255)
 ORANGE = (255, 100, 0)
-TEAL = (2255, 205, 205)
+TEAL = (0, 255, 255)
 
 pygame.init()
 W_DIM = 500
@@ -24,6 +25,8 @@ COLS = 10
 font = pygame.font.SysFont('freesansbold.ttf', 35)
 WIN = pygame.display.set_mode((W_DIM, H_DIM))
 pygame.display.set_caption("Tetris")
+
+SHAPES = ['I', 'J', 'L', 'O', 'S', 'T', 'Z']
 
 figures = {
     'I': [[1, 5, 9, 13], [4, 5, 6, 7]],
@@ -53,19 +56,21 @@ class Cell:
         self.width = width
         self.is_occupied = False
         self.tetramino = None
+        self.color = WHITE
 
-    def is_occupied(self, tetramino):
+    def make_occupied(self, tetramino):
         self.is_occupied = True
-        self.tetramino = type
+        self.tetramino = tetramino
+        self.color = colors[tetramino]
 
     def not_occupied(self):
         self.is_occupied = False
-        self.type = None
+        self.tetramino = None
+        self.color = WHITE
 
     def draw(self, win):
-        if self.tetramino == None:
-            pygame.draw.rect(
-                win, WHITE, (self.col * self.width, self.row * self.width, self.width, self.width))
+        pygame.draw.rect(
+            win, self.color, (self.col * self.width, self.row * self.width, self.width, self.width))
 
 
 class Tetramino:
@@ -77,10 +82,28 @@ class Tetramino:
         self.disposition = 0
         self.x_pos = x
         self.y_pos = y
+        self.locked = False
 
     def rotate(self):
-        length = len(self.graphs)
-        self.disposition = (self.disposition + 1) % length
+        self.disposition = (self.disposition + 1) % len(self.graphs)
+
+    def display_tetrmino(self, grid, rows, cols):
+        count = 0
+        for i in range(4):
+            for j in range(4):
+                cell_x = self.x_pos + j
+                cell_y = self.y_pos + i
+                if not (cell_x > cols - 1 or cell_x < 0 or cell_y > rows - 1 or cell_y < 0):
+                    if count in self.graphs[self.disposition]:
+                        cell = grid[cell_y][cell_x]
+                        cell.make_occupied(self.type)
+                    count += 1
+
+    def advance(self, grid, rows, cols):
+        self.y_pos += 1
+
+    def get_color(self):
+        return self.color
 
 
 def make_grid(rows, cols, width):
@@ -100,7 +123,7 @@ def draw_grid(rows, cols, width, win, board_dim, h_dim):
         pygame.draw.line(win, BLACK, (j * width, 0), (j * width, h_dim))
 
 
-def draw(grid, rows, cols, width, win, w_dim, h_dim):
+def draw(grid, rows, cols, width, win, w_dim, h_dim, tetramino):
     win.fill(BLACK)
     for row in grid:
         for cell in row:
@@ -110,6 +133,7 @@ def draw(grid, rows, cols, width, win, w_dim, h_dim):
     win.blit(message, (415, 100))
     score = 0
     update_score(win, score)
+    tetramino.display_tetrmino(grid, rows, cols)
     pygame.display.update()
 
 
@@ -118,18 +142,41 @@ def update_score(win, score):
     win.blit(display_score, (445, 130))
 
 
+def get_shape(available_shapes):
+    if not available_shapes:
+        available_shapes = SHAPES
+    ran_num = randrange(0, len(available_shapes))
+    return available_shapes.pop(ran_num)
+
+
 def main(rows, cols, width, win, w_dim, h_dim):
+    clock = pygame.time.Clock()
     run = True
     grid = make_grid(rows, cols, width)
+    available_shapes = SHAPES
+    current_shape = get_shape(available_shapes)
+    next_shape = get_shape(available_shapes)
+    tetramino = Tetramino(current_shape, 4, 0)
+    moving_tetramino = False
 
     while run:
+        if moving_tetramino:
+            pass
+            #tetramino.advance(grid, rows, cols)
+        else:
+            current_shape = next_shape
+            tetramino = Tetramino(current_shape, 4, 0)
+            next_shape = get_shape(available_shapes)
+            moving_tetramino = True
 
-        draw(grid, rows, cols, width, win, w_dim, h_dim)
+        draw(grid, rows, cols, width, win, w_dim, h_dim, tetramino)
 
         for event in pygame.event.get():
 
             if event.type == pygame.QUIT:
                 run = False
+
+        clock.tick(5)
 
 
 main(ROWS, COLS, WIDTH, WIN, BOARD_WIDTH, H_DIM)
